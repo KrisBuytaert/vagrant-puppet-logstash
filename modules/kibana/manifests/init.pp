f5039c493f077e38f386d9a298dde9deaf0c31fa
Kris Buytaert <sdog@inuits.be>
2012-02-17 22:53:47 +0100

    Misc upstram changes

--- modules/kibana/manifests/init.pp
+++ modules/kibana/manifests/init.pp
@@ -11,17 +11,17 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class kibana($elasticsearch_server ="localhost:9200")
+class kibana($elasticsearch_server ='localhost:9200')
 {
 
-  package {"kibana":
+  package {'kibana':
     ensure => present;
   }
 
   file { '/etc/httpd/conf.d/kibana.conf':
     ensure => 'file',
     group  => '0',
-    mode   => '644',
+    mode   => '0644',
     owner  => '0',
     source => 'puppet:///modules/kibana/kibana.conf',
   }
@@ -29,14 +29,14 @@ class kibana($elasticsearch_server ="localhost:9200")
   file { '/var/vhosts/kibana/html/':
     ensure => 'directory',
     group  => '0',
-    mode   => '777',
+    mode   => '0777',
     owner  => '0',
   }
 
   file { '/var/vhosts/kibana/html/config.php':
     ensure   => 'file',
     group    => '0',
-    mode     => '664',
+    mode     => '0664',
     owner    => '0',
     content  => template('kibana/config.php.erb')
   }
